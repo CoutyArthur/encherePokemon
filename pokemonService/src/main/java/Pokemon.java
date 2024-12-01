90f64f60ff50bb51e6ccb0953661a0e46f6091d8
CoutyArthur <arth.cty@gmail.com>
2024-12-01 03:17:43 +0100

    caractéristique pokemon via api

--- pokemonService/src/main/java/Pokemon.java
+++ pokemonService/src/main/java/Pokemon.java
@@ -1,24 +1,39 @@
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+
 @Entity
 public class Pokemon{
     @Id
     private long id;
+    private String nom;
     private String description;
     private int miseAPrix;
     private int valeurReel;
+    @ElementCollection
+    private ArrayList<String> type = new ArrayList<>();
+    private int stat;
 
-    public Pokemon(){}
+    public Pokemon(){
+    }
 
 
     public long getId() { return id; }
+    public String getNom(){ return nom; }
     public String getDescription() { return description; }
     public int getMiseAPrix() { return miseAPrix; }
     public int getValeurReel() { return valeurReel; }
+    public ArrayList<String> getType() { return type; }
+    public int getStat() {return stat; }
 
     public void setId(long id) { this.id = id; }
+    public void setNom(String nom) { this.nom = nom; }
+
     public void setDescription(String description){ this.description = description; }
     public void setMiseAPrix(int miseAPrix){ this.miseAPrix = miseAPrix; }
     public void setValeurReel (int valeurReel){ this.valeurReel = valeurReel; }
+    public void setType (ArrayList<String> type){ this.type = type; }
+    public void setStat (int stat){ this.stat = stat; }
+    public void addType(String type) { this.type.add(type); }
 
 }
--- pokemonService/src/main/java/PokemonAPI.java
+++ pokemonService/src/main/java/PokemonAPI.java
@@ -14,6 +14,10 @@ import java.net.http.HttpResponse;
 
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Path("/pokemon")
 public class PokemonAPI {
@@ -22,30 +26,164 @@ public class PokemonAPI {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getPokemon(@PathParam("id") long id){
-        //Pokemon pokemon = pokemonManager.getPokemon(id);
-        //return Response.ok(pokemon).build();
-        return fetchPokemonFromApi(1);
+    public Pokemon getPokemon(@PathParam("id") long id){
+        return fetchPokemonFromApi(id);
     }
 
 
-    public Response fetchPokemonFromApi(long id){
+    public Pokemon fetchPokemonFromApi(long id){
+
+        Pokemon pokemon = new Pokemon();
+        String description = getDescription("https://pokeapi.co/api/v2/pokemon-species/"+id);
+        String name = getName("https://pokeapi.co/api/v2/pokemon-species/"+id);
+        ArrayList<String> type = getType("https://pokeapi.co/api/v2/pokemon/"+id);
+        int stat = getStat("https://pokeapi.co/api/v2/pokemon/"+id);
+        int valeurReel = getValeurReel(stat);
+        int miseAPrix = getMiseAPrix(valeurReel);
+        pokemon.setId(id);
+        pokemon.setDescription(description);
+        pokemon.setNom(name);
+        pokemon.setType(type);
+        pokemon.setStat(stat);
+        pokemon.setValeurReel(valeurReel);
+        pokemon.setMiseAPrix(miseAPrix);
+        return pokemon;
+    }
+
+    public String getDescription(String uriPath){
 
         try{
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://pokeapi.co/api/v2/characteristic/" + id))
+                    .uri(URI.create( uriPath))
+                    .build();
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+            String[] descriptionTab = response.body().split(":");
+            String description = " ";
+            for(int i=0; i<descriptionTab.length; i++){
+                if(descriptionTab[i].compareTo("\"fr\",\"url\"") == 0){
+                    description = descriptionTab[i-2].split("\"")[1];
+                    System.out.println(description);
+                    break;
+                }
+            }
+            return description;
+        }
+        catch (Exception e){
+            return null;
+        }
+    }
+    public String getName(String uriPath){
+
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create( uriPath))
                     .build();
 
             HttpResponse<String> response = client.send(request,
                     HttpResponse.BodyHandlers.ofString());
 
-            System.out.println(response.body());
-            return Response.ok(response.body()).build();
+            String[] nameTab = response.body().split("\"names\"");
+            nameTab = nameTab[1].split(":");
+            String name = " ";
+            for(int i=0; i<nameTab.length; i++){
+                if(nameTab[i].compareTo("\"fr\",\"url\"") == 0){
+                    name = nameTab[i+3].split("\"")[1];
+                }
+            }
+            return name;
         }
         catch (Exception e){
             return null;
         }
+    }
+
+    public ArrayList<String> getType(String uriPath){
+
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create( uriPath))
+                    .build();
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            Map<String, String> map = new HashMap<>();
+            map.put("normal", "normal");
+            map.put("fighting", "combat");
+            map.put("flying", "vol");
+            map.put("poison", "poison");
+            map.put("ground", "sol");
+            map.put("rock", "roche");
+            map.put("bug", "insect");
+            map.put("ghost", "spectre");
+            map.put("steel", "metal");
+            map.put("fire", "feu");
+            map.put("water", "eau");
+            map.put("grass", "plante");
+            map.put("electric", "electrique");
+            map.put("psychic", "psy");
+            map.put("ice", "glace");
+            map.put("dragon", "dragon");
+            map.put("dark", "tenebre");
+            map.put("fairy", "fee");
+            map.put("stellar", "stellaire");
+            map.put("unknown", "inconnu");
+
+            ArrayList<String> type = new ArrayList<String>();
+            String[] typeTab = response.body().split("\"types\"");
+            typeTab = typeTab[1].split("\"type\"");
+            for(int i=1; i<typeTab.length; i++){
+                type.add(map.get(typeTab[i].split("\"")[3]));
+            }
+            return type;
+        }
+        catch (Exception e){
+            return null;
+        }
+    }
+
+    public int getStat(String uriPath){
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create( uriPath))
+                    .build();
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            int stat = 0;
+            String[] statTab = response.body().split("\"stats\"");
+            statTab = statTab[1].split("\"base_stat\":");
+            for(int i=1; i<statTab.length; i++){
+                stat += Integer.parseInt(statTab[i].split(",")[0]);
+            }
+            System.out.println(stat);
+            return stat;
+        }
+        catch (Exception e){
+            return 0;
+        }
+    }
+
+    public int getValeurReel(int stat){
+        int maxStat = 7*300;
+        int maxPrix = 4500;
+        System.out.println((stat*maxPrix) / maxStat);
+        return (stat*maxPrix) / maxStat;
+    }
+
+    public int getMiseAPrix(int valeurReel){
+
+        int min = valeurReel - (int)(valeurReel*0.4);
+        int max = valeurReel + (int)(valeurReel*0.4);
+        double random = Math.random();
+        return (int) (random*(max-min)) + min; //interpolation linéaire
 
     }
 }
